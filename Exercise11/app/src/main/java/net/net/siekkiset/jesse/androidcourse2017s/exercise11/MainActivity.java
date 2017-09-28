361449c4dfc058875c6f282f0c130a00992ca81b
Jesse Siekkinen <jesse.siekkinen@gmail.com>
2017-09-28 19:12:56 +0300

    Added custom array adapter and custom class for product. Also added addProductButton to activity_main.

--- Exercise11/app/src/main/java/net/net/siekkiset/jesse/androidcourse2017s/exercise11/MainActivity.java
+++ Exercise11/app/src/main/java/net/net/siekkiset/jesse/androidcourse2017s/exercise11/MainActivity.java
@@ -2,12 +2,35 @@ package net.net.siekkiset.jesse.androidcourse2017s.exercise11;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private ArrayList<Product> demoProducts = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // FIXME: Demo content
+        demoProducts.add(new Product("Milk", 3, 1.75));
+        demoProducts.add(new Product("Butter", 1, 0.99));
+        demoProducts.add(new Product("Cheese", 1, 3.40));
+
+        ArrayAdapter<Product> adapter = new ProductAdapter(this, demoProducts);
+        ListView listView = (ListView)findViewById(R.id.shoppingItemListView);
+        listView.setAdapter(adapter);
+    }
+
+    protected void addProductButtonClicked(View view) {
+
+    }
+
+    protected void deleteProductClicked(View view) {
+
     }
 }
---
+++ Exercise11/app/src/main/java/net/net/siekkiset/jesse/androidcourse2017s/exercise11/Product.java
@@ -0,0 +1,13 @@
+package net.net.siekkiset.jesse.androidcourse2017s.exercise11;
+
+public class Product {
+    public String name;
+    public int count;
+    public double price;
+
+    public Product(String name, int count, double price) {
+        this.name = name;
+        this.count = count;
+        this.price = price;
+    }
+}
