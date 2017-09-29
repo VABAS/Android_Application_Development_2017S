5c71a15e370b710c56c8dac22d538c591ee1bbd8
Jesse Siekkinen <jesse.siekkinen@gmail.com>
2017-09-29 14:48:25 +0300

    Database connection and demo-data creatinon added.

--- Exercise11/app/src/main/java/net/net/siekkiset/jesse/androidcourse2017s/exercise11/MainActivity.java
+++ Exercise11/app/src/main/java/net/net/siekkiset/jesse/androidcourse2017s/exercise11/MainActivity.java
@@ -1,6 +1,7 @@
 package net.net.siekkiset.jesse.androidcourse2017s.exercise11;
 
-import android.app.DialogFragment;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,20 +12,42 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private ArrayList<Product> demoProducts = new ArrayList<>();
+    SQLiteDatabase shoppingListDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        shoppingListDatabase = initDbConnection();
 
-        // FIXME: Demo content
-        demoProducts.add(new Product("Milk", 3, 1.75));
-        demoProducts.add(new Product("Butter", 1, 0.99));
-        demoProducts.add(new Product("Cheese", 1, 3.40));
+        Cursor c = shoppingListDatabase.rawQuery("SELECT * FROM shoppinglist", null);
+        while(c.moveToNext()) {
+            demoProducts.add(new Product(c.getString(0), c.getInt(0), c.getFloat(2)));
+        }
+        c.close();
+        refreshListView();
 
+    }
+
+    private void refreshListView() {
         ArrayAdapter<Product> adapter = new ProductAdapter(this, demoProducts);
         ListView listView = (ListView)findViewById(R.id.shoppingItemListView);
         listView.setAdapter(adapter);
+    }
+
+    protected SQLiteDatabase initDbConnection() {
+        return initDbConnection(false);
+    }
+    protected SQLiteDatabase initDbConnection(boolean addDemoEntries) {
+        SQLiteDatabase db = openOrCreateDatabase("soppingListDb", MODE_PRIVATE, null);
+        db.execSQL("CREATE TABLE IF NOT EXISTS shoppinglist(name VARCHAR, count INTEGER, price REAL);");
+        if (addDemoEntries) {
+            db.execSQL("DELETE FROM shoppinglist;");
+            db.execSQL("INSERT INTO shoppinglist values('Milk', 3, 1.75);");
+            db.execSQL("INSERT INTO shoppinglist values('Butter', 1, 0.99);");
+            db.execSQL("INSERT INTO shoppinglist values('Cheese', 1, 3.4);");
+        }
+        return db;
     }
 
     protected void addProductButtonClicked(View view) {
@@ -32,8 +55,12 @@ public class MainActivity extends AppCompatActivity {
         addNewProductDialog.show(getFragmentManager(), "m");
     }
 
-    protected void addProduct() {
-
+    protected void addProduct(Product product) {
+        shoppingListDatabase.execSQL("INSERT INTO shoppinglist VALUES('"
+                                     + product.name +"', "
+                                     + product.count +", "
+                                     + product.price +");");
+        refreshListView();
     }
 
     protected void deleteProductClicked(View view) {
