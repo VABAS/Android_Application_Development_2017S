8dffd9709f4908b85a9fe83483bb3f609ea0319f
Jesse Siekkinen <jesse.siekkinen@gmail.com>
2017-09-29 22:39:42 +0300

    WIP with product adding dialog.

--- Exercise11/app/src/main/java/net/net/siekkiset/jesse/androidcourse2017s/exercise11/MainActivity.java
+++ Exercise11/app/src/main/java/net/net/siekkiset/jesse/androidcourse2017s/exercise11/MainActivity.java
@@ -1,12 +1,16 @@
 package net.net.siekkiset.jesse.androidcourse2017s.exercise11;
 
+import android.app.Dialog;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -22,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
 
         Cursor c = shoppingListDatabase.rawQuery("SELECT * FROM shoppinglist", null);
         while(c.moveToNext()) {
-            demoProducts.add(new Product(c.getString(0), c.getInt(0), c.getFloat(2)));
+            demoProducts.add(new Product(c.getString(0), c.getInt(1), c.getFloat(2)));
         }
         c.close();
         refreshListView();
@@ -51,8 +55,70 @@ public class MainActivity extends AppCompatActivity {
     }
 
     protected void addProductButtonClicked(View view) {
-        AddNewProductDialog addNewProductDialog = new AddNewProductDialog();
-        addNewProductDialog.show(getFragmentManager(), "m");
+        final Dialog newProductDialog = new Dialog(this);
+        newProductDialog.setContentView(R.layout.add_product_dialog);
+        Button addButton = (Button)newProductDialog.findViewById(R.id.addButton);
+        Button cancelButton = (Button)newProductDialog.findViewById(R.id.cancelButton);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name =
+                        ((AutoCompleteTextView)newProductDialog.findViewById(R.id.nameAutoCompleteTextView))
+                                .getText().toString();
+                String countS =
+                        ((AutoCompleteTextView)newProductDialog.findViewById(R.id.countAutoCompleteTextView))
+                                .getText().toString();
+                String priceS =
+                        ((AutoCompleteTextView)newProductDialog.findViewById(R.id.priceAutoCompleteTextView))
+                                .getText().toString();
+                int count;
+                try {
+                    count = Integer.parseInt(countS);
+                }
+                catch (NumberFormatException e) {
+                    Toast.makeText(
+                            getBaseContext(),
+                            R.string.count_format_error,
+                            Toast.LENGTH_SHORT
+                    ).show();
+                    return;
+                }
+                float price;
+                try {
+                    price = Float.parseFloat(priceS);
+                }
+                catch (NumberFormatException e) {
+                    Toast.makeText(
+                            getBaseContext(),
+                            R.string.price_format_error,
+                            Toast.LENGTH_SHORT
+                    ).show();
+                    return;
+                }
+
+                shoppingListDatabase.execSQL(
+                        "INSERT INTO shoppinglist values('"+name+"', "+count+", "+price+");"
+                );
+                refreshListView();
+                newProductDialog.dismiss();
+                Toast.makeText(
+                        getBaseContext(), R.string.product_added_notify, Toast.LENGTH_SHORT
+                ).show();
+
+            }
+        });
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                newProductDialog.dismiss();
+                Toast.makeText(
+                        getBaseContext(),
+                        R.string.adding_canceled_notify,
+                        Toast.LENGTH_SHORT
+                ).show();
+            }
+        });
+        newProductDialog.show();
     }
 
     protected void addProduct(Product product) {
